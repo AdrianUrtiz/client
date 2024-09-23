e9e79eed360c861aa51ae1ce1aef2773752a31cf
ArturoEstrada0 <arturoestrada301@gmail.com>
2024-09-23 16:25:54 -0600

    navbar y coordinadores

---
+++ data/ofertaAcademicaState.js
@@ -0,0 +1,50 @@
+export const coordinadores = [
+  {
+    Coordinador: "Maestría en Ciencias en Ingeniería Eléctrica",
+    Correo: "posgrado.electrica@morelia.tecnm.mx",
+    Programa: "Ciencias",
+    Modalidad: "Presencial",
+  },
+  {
+    Coordinador: "Maestría en Ciencias en Ingeniería electrónica",
+    Correo: "posgrado.electronica@morelia.tecnm.mx",
+    Programa: "Ciencias",
+    Modalidad: "Presencial",
+  },
+  {
+    Coordinador: "Maestría en Ciencias en Metalurgia",
+    Correo: "posgrado.metalurgia@morelia.tecnm.mx",
+    Programa: "Ciencias",
+    Modalidad: "Presencial",
+  },
+  {
+    Coordinador: "Doctorado en Ciencias en Ingeniería Eléctrica",
+    Correo: "maria.za@morelia.tecnm.mx",
+    Programa: "Ciencias",
+    Modalidad: "Presencial",
+  },
+  {
+    Coordinador: "Doctorado en Ciencias de la Ingeniería",
+    Correo: "doctorado.ciencias@morelia.tecnm.mx",
+    Programa: "Ciencias",
+    Modalidad: "Presencial",
+  },
+  {
+    Coordinador: "Maestría en Ingeniería Administrativa",
+    Correo: "julio.gj@morelia.tecnm.mx",
+    Programa: "Profesionalizante",
+    Modalidad: "Presencial",
+  },
+  {
+    Coordinador: "Maestría en Economía Social y solidaria",
+    Correo: "julio.gj@morelia.tecnm.mx",
+    Programa: "Profesionalizante",
+    Modalidad: "Presencial",
+  },
+  {
+    Coordinador: "Maestría en sistemas computacionales",
+    Correo: "posgrado.sistemas@morelia.tecnm.mx",
+    Programa: "Profesionalizante",
+    Modalidad: "Presencial",
+  },
+];
