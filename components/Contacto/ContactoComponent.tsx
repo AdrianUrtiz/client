68cfe2d60f524ec6a52d147d666e51b6188c3bf0
Roytrexx <133319533+Roytrexx@users.noreply.github.com>
2024-09-23 16:19:51 -0600

    Contacto y logos

--- components/Contacto/ContactoComponent.tsx
+++ components/Contacto/ContactoComponent.tsx
@@ -1,53 +1,64 @@
 import React from "react";
-import { redesSocialesLinks } from "../../utils/footerLinks";
+import { redesSocialesLinks, redesSocLinks } from "../../utils/footerLinks";
 import SectionTitle from "../HomePage/SectionTitle";
 import SectionComponent from "../SectionComponent";
 
 function ContactoComponent() {
   return (
     <SectionComponent>
-      <SectionTitle title="Contáctanos" />
-      <div className="min-w-fit mb-8 px-5">
-        <h3 className="font-semibold text-xl md:text-3xl text-darkBlue">
+      <SectionTitle title='Contáctanos' />
+      <div className='min-w-fit mb-8 px-5'>
+        <h3 className='font-semibold text-xl md:text-3xl text-darkBlue'>
           Información de contacto
         </h3>
-        <ul className="mt-6">
-          <li className="mt-3 flex flex-col md:flex-row">
-            <p className="font-bold text-sm md:text-base text-darkBlue basis-auto md:basis-32">
+        <ul className='mt-6'>
+          <li className='mt-3 flex flex-col md:flex-row'>
+            <p className='font-bold text-sm md:text-base text-darkBlue basis-auto md:basis-32'>
               Dirección:
             </p>
-            <p className="mt-2 md:mt-auto font-medium text-sm md:text-base text-darkBlue/80 basis-[calc(100%-8rem)]">
-              Av. Universidad 1200, Col. Xoco, Ciudad de México, Alcaldía Benito
-              Juárez C.P. 03330
+            <p className='mt-2 md:mt-auto font-medium text-sm md:text-base text-darkBlue/80 basis-[calc(100%-8rem)]'>
+              Av. Tecnologico No. 1500, Col. Lomas de Santiaguito, C.P. 58120,
+              Morelia, Michoacán, México
             </p>
           </li>
-          <li className="mt-3 flex flex-col md:flex-row">
-            <p className="font-bold text-sm md:text-base text-darkBlue basis-auto md:basis-32">
+          <li className='mt-3 flex flex-col md:flex-row'>
+            <p className='font-bold text-sm md:text-base text-darkBlue basis-auto md:basis-32'>
               Teléfono:
             </p>
-            <p className="mt-2 md:mt-auto font-medium text-sm md:text-base text-darkBlue/80 basis-[calc(100%-8rem)]">
-              55 3600 2500
+            <p className='mt-2 md:mt-auto font-medium text-sm md:text-base text-darkBlue/80 basis-[calc(100%-8rem)]'>
+              443 312 1570 EXT. 316
             </p>
           </li>
-          <li className="mt-3 flex flex-col md:flex-row">
-            <p className="font-bold text-sm md:text-base text-darkBlue basis-auto md:basis-32">
+          <li className='mt-3 flex flex-col md:flex-row'>
+            <p className='font-bold text-sm md:text-base text-darkBlue basis-auto md:basis-32'>
               Correo:
             </p>
-            <p className="mt-2 md:mt-auto font-medium text-sm md:text-base text-darkBlue/80 basis-[calc(100%-8rem)]">
-              dpii@tecnm.mx
+            <p className='mt-2 md:mt-auto font-medium text-sm md:text-base text-darkBlue/80 basis-[calc(100%-8rem)]'>
+              <a
+                href='mailto:depi@morelia.tecnm.mx'
+                className='hover:text-darkBlue/95 hover:underline'>
+                depi@morelia.tecnm.mx
+              </a>
             </p>
           </li>
-          <li className="mt-3 flex flex-col md:flex-row">
-            <p className="font-bold text-sm md:text-base text-darkBlue basis-auto md:basis-32">
-              Redes sociales:
+          <li className='mt-3 flex flex-col md:flex-row'>
+            <p className='font-bold text-sm md:text-base text-darkBlue basis-auto md:basis-32'>
+              Responsable:
             </p>
-            <span className="mt-2 md:mt-auto basis-[calc(100%-8rem)] flex space-x-3">
-              {redesSocialesLinks.map(({ icon, href }, idx) => (
+            <p className='mt-2 md:mt-auto font-medium text-sm md:text-base text-darkBlue/80 basis-[calc(100%-8rem)]'>
+              Héctor Javier Vergara Hernández
+            </p>
+          </li>
+          <li className='mt-3 flex flex-col md:flex-row'>
+            <p className='font-bold text-sm md:text-base text-darkBlue basis-auto md:basis-32'>
+              Redes:
+            </p>
+            <span className='mt-2 md:mt-auto basis-[calc(100%-8rem)] flex space-x-3'>
+              {redesSocLinks.map(({ icon, href }, idx) => (
                 <a
                   href={href}
                   key={idx}
-                  className="font-bold bg-slate-50 h-6 w-6 rounded-sm text-center align-middle text-base md:text-lg text-darkBlue/80 hover:text-darkBlue"
-                >
+                  className='font-bold bg-slate-50 h-6 w-6 rounded-sm text-center align-middle text-base md:text-lg text-darkBlue/80 hover:text-darkBlue'>
                   <i className={icon}></i>
                 </a>
               ))}
@@ -56,14 +67,14 @@ function ContactoComponent() {
         </ul>
       </div>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1881.2634253051094!2d-99.12926571533876!3d19.43283898582782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff758ac697bf%3A0x6a92c70db2edc680!2sTecnol%C3%B3gico%20Nacional%20de%20M%C3%A9xico!5e0!3m2!1ses!2smx!4v1652671775307!5m2!1ses!2smx"
-        className="border-0 w-full h-[30rem] px-5"
+        src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15064.486289314692!2d-101.1908608!3d19.726336!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842d0e40be8f4d3b%3A0xf001c256ae856856!2sInstituto%20Tecnol%C3%B3gico%20de%20Morelia!5e0!3m2!1ses!2smx!4v1652671775307!5m2!1ses!2smx'
+        className='border-0 w-full h-[30rem] px-5'
         allowFullScreen={true}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
+        loading='lazy'
+        referrerPolicy='no-referrer-when-downgrade'
       />
     </SectionComponent>
-  );
+  )
 }
 
 export default ContactoComponent;
--- utils/footerLinks.ts
+++ utils/footerLinks.ts
@@ -1,8 +1,36 @@
 const redesSocialesLinks = [
-  { icon: "bx bxl-facebook", href: "/" },
-  { icon: "bx bxl-twitter", href: "/" },
-  { icon: "bx bxl-instagram", href: "/" },
-];
+  {
+    icon: 'bx bxl-facebook',
+    href: 'https://www.facebook.com/ITMoreliaOficial',
+  },
+  {
+    icon: 'bx bxl-twitter',
+    href: 'https://x.com/itmoficial?t=-HbzZ_NQmv7A8jT7U5ovDw&s=08 ',
+  },
+  {
+    icon: 'bx bxl-instagram',
+    href: 'https://www.instagram.com/tecnm_itmorelia/',
+  },
+  {
+    icon: 'bx bxl-tiktok',
+    href: 'https://www.tiktok.com/@tecnm_campus_morelia?is_from_webapp=1&sender_device=pc',
+  },
+  {
+    icon: 'bx bxl-youtube',
+    href: 'https://www.youtube.com/@TecNMCampusMorelia',
+  },
+]
+
+const redesSocLinks = [
+  {
+    icon: 'bx bxl-facebook',
+    href: 'https://www.facebook.com/profile.php?id=100008429228616',
+  },
+  {
+    icon: 'bx bxl-linkedin',
+    href: 'https://www.linkedin.com/in/hector-javier-vergara-hern%C3%A1ndez-8600a376/',
+  },
+]
 
 const conocenosLinks = [
   { text: "Oferta Académica", href: "/planEstudios" },
@@ -20,11 +48,11 @@ const serviciosLinks = [
 
 const contactanosLinks = [
   {
-    text: "Av. Universidad 1200, Col. Xoco, Ciudad de México, Alcaldía Benito Juárez C.P. 03330",
-    icon: "bx bxs-home",
+    text: ' Av. Tecnologico No. 1500, Col. Lomas de Santiaguito, C.P. 58120, Morelia, Michoacán, México',
+    icon: 'bx bxs-home',
   },
-  { text: "dpii@tecnm.mx", icon: "bx bx-envelope" },
-  { text: "55 3600 2500", icon: "bx bxs-phone" },
-];
+  { text: 'depi@morelia.tecnm.mx', icon: 'bx bx-envelope' },
+  { text: '443 312 1570 EXT. 316', icon: 'bx bxs-phone' },
+]
 
-export { redesSocialesLinks, conocenosLinks, serviciosLinks, contactanosLinks };
+export { redesSocialesLinks, conocenosLinks, serviciosLinks, contactanosLinks, redesSocLinks };
