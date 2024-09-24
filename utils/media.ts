6c2c4cdfd4a5d201e91d0aca7db3ce820c63fd6b
Roytrexx <133319533+Roytrexx@users.noreply.github.com>
2024-09-24 13:45:07 -0600

    cambios arturo

--- utils/media.ts
+++ utils/media.ts
@@ -1,27 +1,28 @@
-const PDF = "/pdf.png";
-const WORD = "/word.png";
-const FILE = "/file.png";
-const ZIP_RAR = "/rar.png";
-const EXCEL = "/excel.png";
-const PICTURE = "/picture.png";
-const TECNM_ICO = "/tecnm.ico";
-const TECNM_LOGO = "/tecnm-white.png";
-const DPII_LOGO = "/dpii.png";
-const DPII_CHATBOT = "/dpii-chatbot.png";
-const TECNM_WHITE_LOGO = "/tecnm-white-logo.png";
-const DPII_WHITE_LOGO = "/dpii-white-logo.png";
-const CONVOCATORIA_BG = "/convocatorias.jpg";
-const CONVOCATORIA_CONACYT_BG = "/convocatoriaConacyt.jpg";
-const CONVOCATORIA_TECNM_BG = "/convocatoriaTecnm.jpg";
-const NORMATECA_BG = "/normateca2020.jpg";
-const NOTICIAS_BG = "/noticias.jpg";
-const DIRECTORIO_BG = "/directorio.jpg";
-const ORGANIGRAMA_BG = "/organigrama.jpg";
-const PROFILE_IMG = "/profile.jpg";
-const ITM = "/logo-itm.png"; // Nueva imagen
-const RIZOMA = "/rizoma.png"; // Nueva imagen
-const EDUCACION = "/LogoBlancoGrande.png"; // Nueva imagen
-const CVU_TECNM = "/cvu-tecnm.png"; // Nueva imagen
+const PDF = '/pdf.png'
+const WORD = '/word.png'
+const FILE = '/file.png'
+const ZIP_RAR = '/rar.png'
+const EXCEL = '/excel.png'
+const PICTURE = '/picture.png'
+const TECNM_ICO = '/tecnm.ico'
+const TECNM_LOGO = '/tecnm-white.png'
+const DPII_LOGO = '/dpii.png'
+const DPII_CHATBOT = '/dpii-chatbot.png'
+const TECNM_WHITE_LOGO = '/tecnm-white-logo.png'
+const DPII_WHITE_LOGO = '/dpii-white-logo.png'
+const CONVOCATORIA_BG = '/convocatorias.jpg'
+const CONVOCATORIA_CONACYT_BG = '/convocatoriaConacyt.jpg'
+const CONVOCATORIA_TECNM_BG = '/convocatoriaTecnm.jpg'
+const NORMATECA_BG = '/normateca2020.jpg'
+const NOTICIAS_BG = '/noticias.jpg'
+const DIRECTORIO_BG = '/directorio.jpg'
+const ORGANIGRAMA_BG = '/organigrama.jpg'
+const PROFILE_IMG = '/profile.jpg'
+const ITM = '/logo-itm.png' // Nueva imagen
+const RIZOMA = '/rizoma.png' // Nueva imagen
+const EDUCACION = '/LogoBlancoGrande.png' // Nueva imagen
+const CVU_TECNM = '/cvu-tecnm.png' // Nueva imagen
+const LOGOSNAVBAR = '/logos-navbar.png' // Nueva imagen
 
 const MEDIA = {
   PDF,
@@ -44,10 +45,10 @@ const MEDIA = {
   DIRECTORIO_BG,
   ORGANIGRAMA_BG,
   PROFILE_IMG,
-  RIZOMA, 
-  EDUCACION, 
-  CVU_TECNM, 
+  RIZOMA,
+  CVU_TECNM,
   ITM,
-};
+  LOGOSNAVBAR,
+}
 
-export { MEDIA };
+export { MEDIA }
