aafd3c2e85e9ea6915a71580950c8e76f110b96d
AdrianUrtiz <116136643+AdrianUrtiz@users.noreply.github.com>
2024-09-24 09:39:39 -0600

    links actualizados

--- utils/navLinks.ts
+++ utils/navLinks.ts
@@ -1,18 +1,25 @@
 const navTopLinks = [
-  { text: 'Inicio', href: '/' },
-  { text: 'Oferta Nacional', href: '/planEstudios' },
-  { text: 'Oferta Morelia', href: 'https://www.morelia.tecnm.mx/#/oferta-educativa/posgrados' },
-  { text: 'Contacto', href: '/contacto' },
-  
+  { text: "Inicio", href: "/" },
   {
-    text: 'Convocatorias',
-    href: '/convocatorias',
+    text: "Oferta Nacional",
+    href: "/planEstudios",
+  },
+  {
+    text: "Oferta Morelia",
+    href: "https://www.morelia.tecnm.mx/#/oferta-educativa/posgrados",
+    target: "_blank",
+    rel: "noreferrer noopener",
+  },
+  { text: "Contacto", href: "/contacto" },
+
+  {
+    text: "Convocatorias",
+    href: "/convocatorias",
     subLinks: [
-      { text: 'Todas las Convocatorias', href: '/convocatorias' },
-      { text: 'Todos los Coordinadores', href: '/coordinadores/coordinadores' },
-    
+      { text: "Todas las Convocatorias", href: "/convocatorias" },
+      { text: "Todos los Coordinadores", href: "/coordinadores/coordinadores" },
     ],
   },
-]
+];
 
-export { navTopLinks }
+export { navTopLinks };
